7205fd7635f08c9b4568b3bf6f1448ba26b565de
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2013-12-22 19:09:38 -0700

    added about_settings/build.sbt and Build.scala

--- about_settings/build.sbt
+++ about_settings/build.sbt
@@ -1,9 +1,10 @@
-name := "About Setting"
+name := "About Settings"
 
 version := "0.1-SNAPSHOT"
 
 scalaVersion := "2.10.1"
 
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+  "org.scalatest" %% "scalatest" % "1.9.1" % "test",
+  "joda-time" % "joda-time" % "2.2"
 )
--- about_settings/project/Build.scala
+++ about_settings/project/Build.scala
@@ -1,21 +1,24 @@
 import sbt._
 import Keys._
+import org.joda.time._
 
 object SettingsBuild extends Build {
 
   val sampleKeyA = SettingKey[String]("sample-a", "demo key A")
-  val sampleKeyB = SettingKey[String]("sample-b", "demo key B")
+  val sampleKeyB = TaskKey[String]("sample-b", "demo key B")
   val sampleKeyC = SettingKey[String]("sample-c", "demo key C")
   val sampleKeyD = SettingKey[String]("sample-d", "demo key D")
 
   override lazy val settings = super.settings ++
     Seq(
-      sampleKeyA := "A: in Build.settings in Build.scala", resolvers := Seq(),
+      sampleKeyA := "A: in Build.settings in Build.scala", 
+      resolvers := Seq(),
       sourceDirectories in (ThisProject, Compile) ~= {srcDirs => srcDirs filter(!_.getAbsolutePath.endsWith("src/main/scala"))},
-      name := "About Settings Project"
+      sampleKeyB := {new DateTime().toString}
+      sampleKeyC := {she  
     )
 
   lazy val root = Project(id = "about_settings",
     base = file("."),
     settings = Project.defaultSettings)
-}
+}àcd 
